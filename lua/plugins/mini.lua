e6e706faf5ab1c3df0dbb5f1d88656efdcd60541
Daniel <daniel@asymmetric.info>
2024-10-24 10:22:51 +0530

    updating which key

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,73 +1,75 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
-  event = "VeryLazy",
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+	"echasnovski/mini.nvim",
+	event = "VeryLazy",
+	config = function()
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
 
-    require('mini.files').setup({
-      window = {
-        preview = true
-      }
-    })
+		require("mini.files").setup({
+			window = {
+				preview = true,
+			},
+		})
 
-    require('mini.diff').setup {}
+		require("mini.diff").setup({})
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'  - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup({
-      mappings = {
-        add = "gsa",            -- Add surrounding in Normal and Visual modes
-        delete = "gsd",         -- Delete surrounding
-        find = "gsf",           -- Find surrounding (to the right)
-        find_left = "gsF",      -- Find surrounding (to the left)
-        highlight = "gsh",      -- Highlight surrounding
-        replace = "gsr",        -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    })
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'  - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.surround").setup({
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		})
 
-    -- comments
-    require('mini.comment').setup()
+		-- comments
+		require("mini.comment").setup()
 
-    -- buf remove
-    require('mini.bufremove').setup()
+		-- buf remove
+		require("mini.bufremove").setup()
 
-    -- pairs
-    require('mini.pairs').setup()
+		-- pairs
+		require("mini.pairs").setup()
 
-    -- indenscope
-    require('mini.indentscope').setup({
-      symbol = "│",
-      options = { try_as_border = true },
-    })
+		-- indenscope
+		require("mini.indentscope").setup({
+			symbol = "│",
+			options = { try_as_border = true },
+		})
 
-    -- disabling mini indentscope for specific file types
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
-      },
-      callback = function()
-        vim.b.miniindentscope_disable = true
-      end,
-    })
-  end,
+		require("mini.hipatterns").setup()
+
+		-- disabling mini indentscope for specific file types
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+	end,
 }
